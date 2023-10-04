17a45002a2ecff85688e9d66ab743d32b632faa9
Josh Moore <jmjoshua7465@gmail.com>
2023-10-04 12:07:13 -0600

    Initial Commit

---
+++ BluetoothLEPlayground/BluetoothLEPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothLEPlaygroundApp.swift
+//  BluetoothLEPlayground
+//
+//  Created by Joshua Moore on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothLEPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BluetoothLEPlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BluetoothLEPlayground
+//
+//  Created by Joshua Moore on 10/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
